130e676b6b33a347a973269ca161e2ec8288dac7
Mahmood Moghimi <mahmood@moghimi.net>
2021-09-21 00:53:57 +0430

    Update ConvertExtensions.cs

--- extensions/ConvertExtensions.cs
+++ extensions/ConvertExtensions.cs
@@ -3,49 +3,41 @@ using Newtonsoft.Json.Serialization;
 
 namespace Moghimi.Core.Json
 {
-	public static class ConvertExtensions
-	{
-		public static string ToJson(this object Object)
-		{
+    public static class ConvertExtensions
+    {
+        //A simple object extention to convert its data to json
+        public static string ToJson(this object Object)
+        {
+            if (Object != null)
+                return JsonConvert.SerializeObject(Object, 
+                    //Use Formatting.Indented for much more readable:
+                    Formatting.None,
+                    new JsonSerializerSettings()
+                    {
+                        PreserveReferencesHandling = PreserveReferencesHandling.None,
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        ContractResolver = new CustomResolver(),
+                    });
+            else
+                return null;
+        }
 
-			if (Object != null)
-				return JsonConvert.SerializeObject(Object, 
-					Formatting.None,
-					new JsonSerializerSettings()
-					{
-						PreserveReferencesHandling = PreserveReferencesHandling.None,
-						ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-						ContractResolver = new CustomResolver(),
-					});
-			else
-				return null;
-		}
-
-
-		public static string ToJsonIgnoreNull(this object Object)
-		{
-
-			if (Object != null)
-				return JsonConvert.SerializeObject(Object,
-					Formatting.None,
-					new JsonSerializerSettings()
-					{
-						PreserveReferencesHandling = PreserveReferencesHandling.None,
-						ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-						ContractResolver = new CustomResolver(),
-						NullValueHandling = NullValueHandling.Ignore,
-					});
-			else
-				return null;
-		}
-
-		public static long AsLongDateTime(this string str)
-		{
-			if (string.IsNullOrEmpty(str))
-				return 0;
-
-			long.TryParse(str.Replace("/", "").Replace(":", "").Replace(" ", ""), out long res);
-			return res;
-		}
-	}
+        //Same as above extenstion but null values with be ignored
+        public static string ToJsonIgnoreNull(this object Object)
+        {
+            if (Object != null)
+                return JsonConvert.SerializeObject(Object,
+                    //Use Formatting.Indented for much more readable:
+                    Formatting.None,
+                    new JsonSerializerSettings()
+                    {
+                        PreserveReferencesHandling = PreserveReferencesHandling.None,
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        ContractResolver = new CustomResolver(),
+                        NullValueHandling = NullValueHandling.Ignore,
+                    });
+            else
+                return null;
+        }
+    }
 }
